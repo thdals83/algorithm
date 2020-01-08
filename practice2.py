e2c35e6e65e1661ae9d548f295a5b448dc233e9f
thdals83 <thdals83@naver.com>
2020-01-08 14:28:04 +0900

    배열 비교

--- practice2.py
+++ practice2.py
@@ -1,25 +1,43 @@
-'''
-문자열 s를 숫자로 변환한 결과를 반환하는 함수작성
-- s의 길이는 1 이상 5이하
-- s의 맨앞에는 부호(+, -)가 올 수 있음
-- s는 부호와 숫자로만 이루어져있음
-- s는 0으로 시작하지 않음
-'''
+import random
 
-def Change(s):
-    n=int(s)
-    return n
+def make_Array():
+    list=[[1,1,1,0,0,0],
+          [0,1,0,0,0,0],
+          [1,1,1,0,0,0],
+          [0,0,2,4,4,0],
+          [0,0,0,2,0,0],
+          [0,0,1,2,4,0]]
+    return list
 
-def Input():
-    while True:
-        string1 = input("입력:")
-        if(string1[0]=='0'):print("다시 입력하세요(첫번재 자리 0으로 시작 불가)")
-        elif(1>len(string1) or len(string1)>5):print("다시입력하세요 (문자열길이 1이상 5이하)")
-        else:break
+def Plus(a,x,y):
+    count=0
+    max=0
+    j=y
+    while count<3:
+        max=max+a[x][j]+a[x+2][j]
+        j=j+1
+        count=count+1
 
-    return string1
+    max=max+a[x+1][y+1]
 
+    return max
+
+def compare_Array(arr):
+    a=0
+    for i in range(len(arr)-2):
+        for j in range(len(arr[0])-2):
+            if(a==0):
+                max=Plus(arr,i,j)
+                tmp=max
+                a=1
+            max = Plus(arr, i, j)
+            if(tmp<max):
+                tmp=max
+
+    return tmp
 
 if __name__=="__main__":
-    string=Input()
-    print(Change(string))
+    list=make_Array()
+    maximum=compare_Array(list)
+
+    print(maximum)
