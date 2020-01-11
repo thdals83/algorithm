a61d6b51cc59bedba93fa4b7578fe70d6920011e
thdals83 <thdals83@naver.com>
2020-01-11 14:03:19 +0900

    소수 찾기

--- practice2.py
+++ practice2.py
@@ -1,35 +1,19 @@
-import random
-import queue
+def check(x):
+    if(x<2):return False
 
-def made(x):
-    arr=[]
-    for i in range(x): arr.append(random.randint(1,300))
-    return arr
+    for i in range(2,x):
+        if(x%i==0):
+            return False
+    return True
 
-def radix_sort(list,n):
-    baskets=[]
-    tmp=0
-    factor=1
-    for i in range(10): baskets.append(queue.Queue()) #10인 이유는 0~9까지의 버킷
+def confirm(x):
+    for i in range(x + 1):
+        if (check(i) == True):
+            if(i==x):return True
 
-    for i in range(4): #4인이유는 일단 4개의 자리수 칸들
-        for j in range(n):
-            x=int((list[j]//factor)%10)
-            baskets[x].put(list[j])
-
-        for k in range(10):
-            while baskets[k].empty()==False:
-                if tmp>n-1: tmp=0
-                list[tmp]=baskets[k].get()
-                tmp=tmp+1
-        factor=factor*10
-
-    list.reverse()
-    print(list)
-
+    return False
 
 if __name__=="__main__":
-    x = int(input("개수 입력:"))
-    input=made(x)
-    print(input)
-    radix_sort(input,x)
+    x=int(input())
+    if(confirm(x)):print("소수입니다.")
+    else:print("소수가 아닙니다.")
