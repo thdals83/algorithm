f5a98f8f1a01e70f4c503616e4362b12f685aca0
thdals83 <thdals83@naver.com>
2020-01-12 11:04:55 +0900

    소수 찾기2

--- practice2.py
+++ practice2.py
@@ -1,19 +1,21 @@
-def check(x):
-    if(x<2):return False
-
-    for i in range(2,x):
-        if(x%i==0):
-            return False
-    return True
-
 def confirm(x):
-    for i in range(x + 1):
-        if (check(i) == True):
-            if(i==x):return True
+    global count
+    count=0
 
-    return False
+    list=[False,False]+[True]*(x-1)
+    index=[]
+
+    for i in range(2,x+1):
+        count=count+1
+        if list[i]:
+            index.append(i)
+            for j in range(2*i,x+1,i):
+                if(x==j):return False
+                list[j]=False
+    return True
 
 if __name__=="__main__":
     x=int(input())
     if(confirm(x)):print("소수입니다.")
     else:print("소수가 아닙니다.")
+    print(count)
