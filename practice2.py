4357ef67f2c53b772c87ef0267ed128aad546be6
thdals83 <thdals83@naver.com>
2020-01-16 09:21:21 +0900

    9단계(수학1) 2869번 (달팽이 )

--- practice2.py
+++ practice2.py
@@ -1,18 +1,8 @@
-num=int(input())
+a,b,c=map(int,input().split())
 
-i=1
-a=0
-while True:
-    b=a+i
-    if(a<num<=b):break
-    i=i+1
-    a=b
-
-a=a+1
-mo=num-a+1
-ja=i-mo+1
-
-if(i%2==0):
-    print("%d/%d" % (mo, ja))
+n=(c-b)/(a-b)
+if(n==int(n)):
+    print(int(n))
 else:
-    print("%d/%d" % (ja, mo))
+    print(int(n)+1)
+
