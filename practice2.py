7c56ce41698933424525652baedc31276949e8d0
thdals83 <thdals83@naver.com>
2020-01-17 10:57:23 +0900

    9단계(수학1) 2775번 (부녀회장)

--- practice2.py
+++ practice2.py
@@ -1,12 +1,21 @@
-x=int(input())
+arr=[[0]*14 for i in range(15)]
 
-for i in range(x):
-    h,w,n=map(int,input().split())
+for i in range(14):
+    arr[0][i]=i+1
+    arr[i][0]=1
+arr[14][0]=1
 
-    if(n%h==0):
-        print("%d%02d"%(h,n//h))
-    else:
-        print("%d%02d" % (n % h, n // h + 1))
+for i in range(1,15):
+    for j in range(1,14):
+        arr[i][j]=arr[i][j-1]+arr[i-1][j]
+
+
+t=int(input())
+for i in range(t):
+    a = int(input())
+    b = int(input())
+    print(arr[a][b-1])
+
 
 
 
