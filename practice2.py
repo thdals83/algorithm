f189ff83e7e3598adbadd3ad1630211d163d7010
thdals83 <thdals83@naver.com>
2020-01-22 14:05:11 +0900

    9단계(수학1) 1929번 (소수)

--- practice2.py
+++ practice2.py
@@ -1,35 +1,18 @@
-def check(x):
-    if(x<2):return False
+def confirm(x,y):
 
-    for i in range(2,x):
-        if(x%i==0):
-            return False
-    return True
-
-def confirm(x):
-    for i in range(x + 1):
-        if (check(i) == True):
-            if(i==x):return True
-    count=0
-
-    return False
-    list=[False,False]+[True]*(x-1)
+    list=[False,False]+[True]*(y-1)
     index=[]
 
-    for i in range(2,x+1):
-        if list[i]:
+    for i in range(2,y+1):
+        if list[i]==True:
             index.append(i)
-            for j in range(2*i,x+1,i):
-                if(x==j):return False
-                list[j]=False
-    return True
+            for j in range(2 * i, y + 1, i):
+                list[j] = False
+
+    for i in range(x,y+1):
+        if(list[i]==True):
+            print(i)
 
 if __name__=="__main__":
-    x=int(input())
-    count=0
-    a=list(map(int,input().split()))
-    for i in range(len(a)):
-        if(confirm(a[i])==True):
-            count=count+1
-
-print(count)
+    x,y=map(int,input().split())
+    confirm(x,y)
