e52207b74afc1b37fe5667f665f81917e667dc28
thdals83 <thdals83@naver.com>
2020-01-23 11:02:22 +0900

    9단계(수학1) 4948번 (베르트랑 공준)

--- practice2.py
+++ practice2.py
@@ -2,17 +2,22 @@ def confirm(x,y):
 
     list=[False,False]+[True]*(y-1)
     index=[]
-
+    count=0
     for i in range(2,y+1):
         if list[i]==True:
             index.append(i)
             for j in range(2 * i, y + 1, i):
                 list[j] = False
 
-    for i in range(x,y+1):
+    for i in range(x+1,y+1):
         if(list[i]==True):
-            print(i)
+            count=count+1
+
+    print(count)
 
 if __name__=="__main__":
-    x,y=map(int,input().split())
-    confirm(x,y)
+    while True:
+        x=int(input())
+        y=2*x
+        if(x==0):break
+        confirm(x,y)
