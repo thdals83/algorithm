132a8fce727aea9d8b1c8d1a13da4a977d400cbc
thdals83 <thdals83@naver.com>
2020-01-23 14:50:19 +0900

    9단계(수학1) 9020번 (골드바흐)

--- practice2.py
+++ practice2.py
@@ -1,23 +1,15 @@
-def confirm(x,y):
+z = 10001
+list = [False, False] + [True] * (z - 1)
+for i in range(2, z + 1):
+    if list[i] == True:
+        for j in range(2 * i, z + 1, i):
+            list[j] = False
 
-    list=[False,False]+[True]*(y-1)
-    index=[]
-    count=0
-    for i in range(2,y+1):
-        if list[i]==True:
-            index.append(i)
-            for j in range(2 * i, y + 1, i):
-                list[j] = False
-
-    for i in range(x+1,y+1):
-        if(list[i]==True):
-            count=count+1
-
-    print(count)
-
-if __name__=="__main__":
-    while True:
-        x=int(input())
-        y=2*x
-        if(x==0):break
-        confirm(x,y)
+count=int(input())
+for _ in range(count):
+    x = int(input())
+    y=x//2
+    for j in range(y,1,-1):
+        if(list[x-j]==True and list[j]==True):
+            print(j,x-j)
+            break
