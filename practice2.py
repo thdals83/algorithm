ad287d4c4445617b38ecf588a249b8bd20c1a1e4
thdals83 <thdals83@naver.com>
2020-01-24 10:52:05 +0900

    9단계(수학1) 1085번 (직사각형)

--- practice2.py
+++ practice2.py
@@ -1,15 +1,6 @@
-z = 10001
-list = [False, False] + [True] * (z - 1)
-for i in range(2, z + 1):
-    if list[i] == True:
-        for j in range(2 * i, z + 1, i):
-            list[j] = False
+x,y,w,h=map(int,input().split())
 
-count=int(input())
-for _ in range(count):
-    x = int(input())
-    y=x//2
-    for j in range(y,1,-1):
-        if(list[x-j]==True and list[j]==True):
-            print(j,x-j)
-            break
+q=w-x
+e=h-y
+
+print(min(x,y,q,e))
