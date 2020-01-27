891600aa5011b1cfc1390207f1f7926936de1c56
thdals83 <thdals83@naver.com>
2020-01-27 10:39:40 +0900

    9단계(수학1) 3053번 (택시)

--- practice2.py
+++ practice2.py
@@ -1,12 +1,6 @@
-while True:
-    a, b, c = map(int, input().split())
-    if (a == 0 and b == 0 and c == 0): break
+import math
 
-    if (a**2 +b**2==c**2):
-        print("right")
-    elif(a**2 +c**2==b**2):
-        print("right")
-    elif(c**2 +b**2==a**2):
-        print("right")
-    else:
-        print("wrong")
+x=int(input())
+y=(x**2)*math.pi
+print("%.6f"%y)
+print("%.6f"%((x**2)*2))
