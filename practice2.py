03575a409e5fcc9152d6987245da639380d78778
thdals83 <thdals83@naver.com>
2020-01-30 18:33:59 +0900

    9단계(수학1) 15997번 (승부 예측) - 연습1

--- practice2.py
+++ practice2.py
@@ -1,16 +1,29 @@
-def make(i,j):
-    while(i!=0):
-        if (i % 3 == 1 and j % 3 == 1):
-            print(" ", end="")
-            return None
-        i=i//3
-        j=j//3
-    print("*", end="")
+import sys
+list=list(input().split()) #나라 입력
+n=([0]*4) #각 나라의 점수를 만들 리스트 4개
+
+for _ in range(6):
+    a, b, x, y, z = input().split() #입력
+    x = float(x)
+    y = float(y)
+    z = float(z)
+
+    for i in range(4): # 입력한 국가와 비교해 승점을 입력해줌
+        if(a==list[i]):
+            if(y==0):
+                n[i] = n[i] + x
+            else:
+                n[i]=n[i]+((x+y)/2)
+        if(b==list[i]):
+            if(y==0):
+                n[i] = n[i] + z
+            else:
+                n[i]=n[i]+((y+z)/2)
 
 
-if __name__=="__main__":
-    x=int(input())
-    for i in range(0,x):
-        for j in range(0,x):
-            make(i,j)
-        print("")
+for i in range(4):  #승점 출력
+    print(n[i]/3)
+
+s=sorted(n,reverse=True)
+
+
