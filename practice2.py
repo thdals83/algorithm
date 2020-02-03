45e7d0be2aee1260f5909760c930a02cb418be7d
thdals83 <thdals83@naver.com>
2020-02-03 13:48:07 +0900

    9단계(수학1) 11729번 (하노이 탑)

--- practice2.py
+++ practice2.py
@@ -1,29 +1,14 @@
-import sys
-list=list(input().split()) #나라 입력
-n=([0]*4) #각 나라의 점수를 만들 리스트 4개
-
-for _ in range(6):
-    a, b, x, y, z = input().split() #입력
-    x = float(x)
-    y = float(y)
-    z = float(z)
-
-    for i in range(4): # 입력한 국가와 비교해 승점을 입력해줌
-        if(a==list[i]):
-            if(y==0):
-                n[i] = n[i] + x
-            else:
-                n[i]=n[i]+((x+y)/2)
-        if(b==list[i]):
-            if(y==0):
-                n[i] = n[i] + z
-            else:
-                n[i]=n[i]+((y+z)/2)
+def hanoi(n,frm,tmp,to):
+    if (n==1):print(frm,to)
+    else:
+        hanoi(n-1,frm,to,tmp)
+        print(frm,to)
+        hanoi(n-1,tmp,frm,to)
 
 
-for i in range(4):  #승점 출력
-    print(n[i]/3)
-
-s=sorted(n,reverse=True)
 
 
+if __name__=="__main__":
+    x=int(input())
+    print((2**x)-1)
+    hanoi(x,1,2,3)
